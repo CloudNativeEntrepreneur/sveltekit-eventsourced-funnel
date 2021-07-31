8c2d83038a57434981133a70f0dbc9f47f700f5a
Patrick Lee Scott <pat@patscott.io>
2021-07-31 15:45:57 -0500

    fix: make steps ValueObjects (immutable)
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- src/lib/steps.js
+++ src/lib/steps.js
@@ -1,40 +1,42 @@
+import { ValueObject } from "./eventsourcing/sourced"
+
 // declared backwards so they can reference eachother
-const thankyou = {
+const thankyou = ValueObject({
   url: '/thank-you',
   name: 'Thank You'
-}
+})
 
-const downsell = {
+const downsell = ValueObject({
   url: '/downsell',
   name: 'Downsell',
   nextStepYes: thankyou.url,
   nextStepNo: thankyou.url
-}
+})
 
-const upsell = {
+const upsell = ValueObject({
   url: '/upsell',
   name: 'Upsell',
   nextStepYes: thankyou.url,
   nextStepNo: downsell.url
-}
+})
 
-const checkout = {
+const checkout = ValueObject({
   url: '/checkout',
   name: 'Checkout',
   nextStep: upsell.url
-}
+})
 
-const oto = {
+const oto = ValueObject({
   url: '/oto',
   name: 'One Time Offer',
   nextStepYes: checkout.url,
   nextStepNo: thankyou.url
-}
+})
 
-const leadMagnet = {
+const leadMagnet = ValueObject({
   url: '/',
   name: 'Lead Magnet',
   nextStep: oto.url
-}
+})
 
 export const steps = [leadMagnet, oto, checkout, upsell, downsell, thankyou]
